ccf59cd9ba19afd031a9e09536a3402443c878aa
Unun0kwaD <pawel.mar.2001@gmail.com>
2022-11-08 19:01:23 +0100

    change 1

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,11 +1,13 @@
 class Bicycle {
 
   // state or field
-  private int gear = 5;
+  public int gear = 5;
   final int data=3;
 
   // behavior or method
+  //comment
   public void braking() {
     System.out.println("Working of Braking");
+    System.out.println("Braking of Working");
   }
 }
